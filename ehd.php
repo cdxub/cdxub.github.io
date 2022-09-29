685cdb54ad59a2a20dece5af8af3cf929eedd207
cdxub <114663375+cdxub@users.noreply.github.com>
2022-09-29 19:46:09 +0600

    main

---
+++ ehd.php
@@ -0,0 +1,38 @@
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="apple-touch-icon" sizes="180x180" href="fav/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="fav/favicon-16x16.png">
+    <link rel="manifest" href="fav/site.webmanifest">
+    <link rel="mask-icon" href="fav/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+<style>
+body {
+    background-color: #121212;
+}
+div.su {
+  color: white;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translateX(-50%) translateY(-50%);
+  -webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+}  
+h1 {
+    font-size: 60px;
+    font-family: Monospace;
+    font-weight: bold;
+    color: #fff;
+}
+p {
+    font-family: Monospace;
+    font-size: 18px;
+    color: #fff;
+}
+a {
+    text-decoration: none;
+    font-family: Monospace;
+}
+</style>
---
+++ mhd.php
@@ -0,0 +1,65 @@
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="apple-touch-icon" sizes="180x180" href="fav/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="fav/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="fav/favicon-16x16.png">
+    <link rel="manifest" href="fav/site.webmanifest">
+    <link rel="mask-icon" href="fav/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+<style>
+body {
+    background-color: #000;
+}
+div.su {
+  color: white;
+  padding: 15px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translateX(-50%) translateY(-50%);
+  -webkit-transform: translate(-50%,-50%);
+  transform: translate(-50%,-50%);
+}  
+h1.l {
+    font-family: Monospace; 
+    margin: auto;
+    margin-left: 4px;
+    color: #fff;
+}
+h1.lm {
+    font-family: Monospace; 
+    margin: auto;
+    margin-left: 4px;
+    color: #fff;
+    font-size: 14px;
+    font-weight: cursive;
+}
+h1.hd {
+    font-family: Monospace, Arial; 
+    margin: auto;
+    margin-left: 4px;
+    font-size: 20px;
+    color: #fff;
+}
+table.banner {
+    width:100%; 
+    border-radius: 3px; 
+    background-color: #040C20;
+    margin-bottom: 5px;
+}
+table.std {
+    width:100%; 
+    height: 60%;
+    border-radius: 3px; 
+    background-color: #040C20;
+    margin-bottom: 5px;
+}
+p {
+    color: #fff;
+    font-size: 12px;
+    margin-inline: 3px;
+    font-family: Monospace;
+    font-weight: bold;
+}
+</style>
